d03db4871ce45258986c468d407c63733048c285
Jenya Lebid <61124137+ljenya@users.noreply.github.com>
2020-10-16 19:26:43 -0700

    Initial Commit

---
+++ Holiday App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Holiday App
+//
+//  Created by Jenya Lebid on 10/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
